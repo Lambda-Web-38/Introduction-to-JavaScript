749ee98789883bf6b0b44d7b78f30fdc6dc039b7
Vital Boisset <vital.boisset@gmail.com>
2020-11-04 07:20:32 -0500

    MVP Reached + CodeGrader Setup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//CodeGrader Setup
+
 /*🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️ Task 1: Warm-up! 🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️*/
 
 /*MAKE SURE TO RETURN ALL OF THE ANSWERS ON THESE TASKS, IF YOU DON'T, THE AUTOGRADER WILL NOT WORK*/
